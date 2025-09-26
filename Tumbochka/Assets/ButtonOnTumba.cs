91b5efe2637988fafc28d42364219adbffc566b3
Midas00777 <vadim.odin3006@gmail.com>
2025-09-26 16:47:57 +0500

    Menu

---
+++ Tumbochka/Assets/ButtonOnTumba.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonOnTumba : MonoBehaviour
+{
+
+  
+    public Animator animPanel;
+
+    public Animator camAnim;
+
+    
+    public void ButtonStart()
+    {
+
+        camAnim.SetBool("selFirst", true);
+        animPanel.SetBool("closePanel", true);
+        Invoke("CallMethodSwapScene", 2f);
+
+    }
+
+    public void ButtonSeting()
+    {
+        camAnim.SetBool("selSecond", true);
+        animPanel.SetBool("closePanel", true);
+        
+    }
+
+    public void ButtonExit()
+    {
+        camAnim.SetBool("selThird", true);
+        animPanel.SetBool("closePanel", true);
+        Invoke("CallMethodQuit", 2f);
+    }
+
+    public void CallMethodQuit()
+    {
+        Application.Quit();
+    }
+
+    public void CallMethodSwapScene()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+}
